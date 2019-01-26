552f6efdd1c7b52148ec5a95b49a2724bc78837c
Antonia Berger <AntoniaBerger@gmx.net>
2019-01-26 22:08:42 +0100

    fixed routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import jsonify
+from flask import jsonify, send_from_directory
+import os
 
+BASE_URL = os.path.abspath(os.path.dirname(__file__))
+CLIENT_APP_FOLDER = os.path.join(BASE_URL, "../angularApp/src")
 
+print('BASE_URL ' + BASE_URL)
+print('CLIENT_APP_FOLDER ' + CLIENT_APP_FOLDER)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -14,7 +19,7 @@ from app import models
 db.init_app(app)
 
 from flask import session
-from app.models import Federal_Territory, Party, Constituency, Result
+from app.models import Federal_Territory, Party, Constituency, Result, Federal_State
 from read_file import read_csv
 
 
@@ -23,25 +28,25 @@ with app.app_context():
 	db.create_all()
 	federal_territory = Federal_Territory()
 	party = Party()
-	
+
 	federal_territories = read_csv(federal_territory)
 	db.session.add(federal_territories)
 	db.session.commit()
 
 
 
-@app.route('/api/federal_territory', methods=['GET'])
-def federal_territories():
+@app.route('/api/federal_states', methods=['GET'])
+def federal_states():
 
     data = []
-    federal_territories = Federal_Territory.query.all()
+    federal_states = Federal_State.query.all()
 
-    for ft in federal_territories:
-    	data.append({'id': ft.id, 'name': ft.name})
+    for fs in federal_states:
+    	data.append({'id': fs.id, 'name': fs.name})
 
     return jsonify({'data': data})
 
-@app.route('/api/federal_territory/<int:federal_state_id>/constituencies', methods=['GET'])
+@app.route('/api/federal_states/<int:federal_state_id>/constituencies', methods=['GET'])
 def constituencies(federal_state_id):
 	constituencies = Constituency.query.filter_by(federal_state_id=federal_state_id).all()
 	data = []
@@ -64,5 +69,11 @@ def electorial_results_party(constituency_id):
      	final_data.append(
      		{ 'partyName': r.party.name, 'firstVote': r.first_vote, 'secondVote': r.second_vote, 'firstVoteTotal': r.first_vote_total, 'secondVoteTotal': r.second_vote_total})
      return jsonify({'data': final_data})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')  # Catch All urls, enabling copy-paste url
+def home(path):
+    return send_from_directory(CLIENT_APP_FOLDER, 'index.html')
+
 
 app.run(debug=True)
