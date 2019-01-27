be49b31aba58ca5467293123fd13b5f0e9391064
Antonia Berger <AntoniaBerger@gmx.net>
2019-01-27 18:01:46 +0100

    FINISHED

--- angularApp/src/app/constituency/constituency.component.ts
+++ angularApp/src/app/constituency/constituency.component.ts
@@ -10,51 +10,76 @@ import { Constituency, Result } from '../data.models'
   styleUrls: ['./constituency.component.css']
 })
 export class ConstituencyComponent implements OnChanges {
-    results: Result[];
+  results: Result[];
+  term: string;
 
   @Input('constituencies') constituencies;
   @Input('federalState_name') federalState_name: string;
 
-  constructor(private constituencyService: ConstituencyService) {}
+  backUpconstituencies: any;
+
+
+  constructor(private constituencyService: ConstituencyService) { }
 
   private constituencyName: string;
 
   ngOnChanges() {
-      this.results = null;
-
+    this.term = '';
+    this.results = null;
+    this.backUpconstituencies = this.constituencies;
   }
 
+  ngOnInit() {
+   this.backUpconstituencies = this.constituencies;
+  }
+
+  abc(event:any)  {
+    console.log(this.backUpconstituencies);
+    this.constituencies = this.filterValues(this.backUpconstituencies, event.target.value);
+  }
+
+
+  filterValues(value: any, input: string){
+      console.log("value ", value);
+      console.log("input ", input);
+   if (input) {
+        input = input.toLowerCase();
+        return value.filter(function (el: any) {
+            console.log('el: ', el);
+            return el.name.toLowerCase().indexOf(input) > -1;
+        })
+    }
+    return value;
+}
   getResult(constituency: Constituency) {
     this.constituencyName = constituency.name;
 
     this.constituencyService.fetchResults(constituency.id).subscribe(results => {
-        this.results = results.data;
-        this.calculateInPercent(this.results);
-        console.log("results " , this.results);
-        }, err => {console.log(err)});
-}
-
-calculateInPercent(results) {
-  var first_total = results[0].firstVoteTotal;
-  var second_total = results[0].secondVoteTotal;
-
-  //first_total = 100 %
-  //first_votes =
-
-  for(var result in results){
-
-
-    var firstVotePercent = (Math.round( (results[result].firstVote * 100 / first_total)* 100)/100).toFixed(2);
-
-
-
-    var secondVotePercent = (Math.round( (results[result].secondVote * 100 / second_total)* 100)/100).toFixed(2);
-
-    results[result]["firstVotePercent"] = firstVotePercent;
-    results[result]["secondVotePercent"] = secondVotePercent;
+      this.results = results.data;
+      this.calculateInPercent(this.results);
+      console.log("results ", this.results);
+    }, err => { console.log(err) });
   }
 
-  console.log(results);
-}
+  calculateInPercent(results) {
+    var first_total = results[0].firstVoteTotal;
+    var second_total = results[0].secondVoteTotal;
+
+    //first_total = 100 %
+    //first_votes =
+
+    for (var result in results) {
+
+
+      var firstVotePercent = (Math.round((results[result].firstVote * 100 / first_total) * 100) / 100).toFixed(2);
+
+
+
+      var secondVotePercent = (Math.round((results[result].secondVote * 100 / second_total) * 100) / 100).toFixed(2);
+
+      results[result]["firstVotePercent"] = firstVotePercent;
+      results[result]["secondVotePercent"] = secondVotePercent;
+    }
+  }
 
 }
--- angularApp/src/app/election-graph/election-graph.component.ts
+++ angularApp/src/app/election-graph/election-graph.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts'
+import { Result } from '../data.models'
 
 @Component({
   selector: 'app-election-graph',
   templateUrl: './election-graph.component.html',
   styleUrls: ['./election-graph.component.css']
 })
-export class ElectionGraphComponent implements OnInit {
+export class ElectionGraphComponent implements OnChanges {
 
-  constructor() { }
+ @Input('results') results: Result[];
 
-  ngOnInit() {
+  public doughnutChartPartyLabels:string[] = [];
+  public doughnutChartFirstVote:number[] = [];
+  public doughnutChartSecondVote:number[] = [];
+
+  public doughnutChartType:string = 'doughnut';
+
+
+
+  constructor() {
+
+
+   }
+
+  ngOnChanges() {
+
+    this.resetResult();
+    for(var result in this.results){
+
+      this.doughnutChartPartyLabels.push(this.results[result].partyName);
+      this.doughnutChartFirstVote.push(this.results[result].firstVote)
+      this.doughnutChartSecondVote.push(this.results[result].secondVote)
+
+
+    }
+    console.log("this.results ", this.results);
   }
+
+
+  resetResult() {
+    this.doughnutChartFirstVote = [];
+    this.doughnutChartSecondVote = [];
+    this.doughnutChartPartyLabels = [];
+}
 
 }
